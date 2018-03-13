da021f50a30c824622e9e26b706fe01d5ebd85ea
Alexander Akulich <akulichalexander@gmail.com>
2018-03-14 00:54:26 +0300

    Add Sailfish OS platform mkspec
    Change-Id: Ib5831000a810c13f8e86dca291c4a6d3dce13c16

---
+++ mkspecs/linux-g++-sailfish/qplatformdefs.h
@@ -0,0 +1,9 @@
+/****************************************************************************
+**
+** Copyright (C) 2017 Alexandr Akulich <akulichalexander@gmail.com>
+**
+** This file is part of the qmake spec of the Qt Toolkit.
+**
+****************************************************************************/
+
+#include "../linux-g++/qplatformdefs.h"
